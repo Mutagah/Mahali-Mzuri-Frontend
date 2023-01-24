860cc64616b0e69ab89990e14978f8cc6d389a31
Ezra Njeru <njerukithinji872@gmail.com>
2023-01-24 15:38:53 +0300

    Simple begining

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,15 @@
 import React from "react";
-export default function AboutPage(){
-  return <>This is the About Page</>;
+export default function AboutPage() {
+  return(
+    <div className="about">
+      <div className="about-details">
+        <h1>ABOUT US</h1>
+        <p>This hotel offers a luxurious stay with all the amenities you need. Enjoy delicious food and drinks in the restaurant, relax in the bar, and get a good night's sleep in one of our comfortable rooms. Our friendly staff will make sure your stay is enjoyable and memorable. We also offer a variety of activities to keep you entertained during your stay. Whether you're looking for a romantic getaway or just want to escape from the hustle and bustle of everyday life, this hotel is the perfect place for you.</p>
+        <button>Explore</button>
+      </div>
+      <div className="about-imgs">
+        <img src="https://imgs.search.brave.com/9EtfaKlkaq4PGTzZX4tGC-tOCr8uFcfWqs5QEEe6nPc/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC55/YjBMMWhsVzdkR2NB/U1Bsb1piNHpBSGFF/OCZwaWQ9QXBp" alt="restaurant" />
+      </div>
+    </div>
+  );
 }
