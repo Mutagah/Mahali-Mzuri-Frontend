3732962c00c3e27f4f75eb045d30280b2c60a60e
Zamzam <Zamzamhassan44@gmail.com>
2023-01-25 11:17:29 +0300

    added the breakfast section to restaurant.js

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -13,6 +13,72 @@ const Restaurant = () => {
         </div>
 
      </section>
+
+
+<section class="speciality" id="speciality">
+
+<h1 class="heading"> our <span>Breakfast</span> </h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <img class="image" src="https://images.unsplash.com/photo-1525804017725-0ac671aea1fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80" alt="" />
+        <div class="content">
+            <img src="" alt="" />
+            <h3>English breakfast</h3>
+            <h3>Ksh.1200</h3>
+            <p>The full or English breakfast is a more elaborate meal and requires more preparation in the dining room before service than other meals. The menu may consist of from two to eight courses</p>
+        </div>
+    </div>
+    <div class="box">
+        <img class="image" src="images/s-img-2.jpg" alt="" />
+        <div class="content">
+            <img src="images/s-2.png" alt="" />
+            <h3>Continental breakfast</h3>
+            <p>The traditional Continental BF consisted simply of hot croissant, brioche or toast, butter and preserves and coffee as the beverage. The current trend in the continental BF menu is towards offering a wider variety of choices.</p>
+        </div>
+    </div>
+    <div class="box">
+        <img class="image" src="images/s-img-3.jpg" alt="" />
+        <div class="content">
+            <img src="" alt="" />
+            <h3>American breakfast</h3>
+            <h3>Ksh.1200</h3>
+            <p>The American Breakfasts also offers multiple courses as a part of the meal, and Is similar to an English Breakfasts, except that American Breakfast does not offer any fish dish on the menu.</p>
+        </div>
+    </div>
+    <div class="box">
+        <img class="image" src="images/s-img-4.jpg" alt="" />
+        <div class="content">
+            <img src="images/s-4.png" alt="" />
+            <h3>Buffet breakfast</h3>
+            <h3>Ksh.1200</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda inventore neque amet ipsa tenetur voluptates aperiam tempore libero labore aut.</p>
+        </div>
+    </div>
+    <div class="box">
+        <img class="image" src="images/s-img-5.jpg" alt="" />
+        <div class="content">
+            <img src="images/s-5.png" alt="" />
+            <h3>Regional breakfast</h3>
+            <h3>Ksh.1200</h3>
+            <p>Other Regional Breakfast like South Indian, North Indian, Japanese, Chinese, Vietnamese etc.</p>
+        </div>
+    </div>
+    <div class="box">
+        <img class="image" src="images/s-img-6.jpg" alt="" />
+        <div class="content">
+            <img src="images/s-6.png" alt="" />
+            <h3>Healty breakfast</h3>
+            <h3>Ksh.1200</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda inventore neque amet ipsa tenetur voluptates aperiam tempore libero labore aut.</p>
+        </div>
+    </div>
+
+</div>
+
+</section>
+
         </div>
     );
 }
