bb0007b74b658cd38944b37b494596537a15ff3a
Zamzam <Zamzamhassan44@gmail.com>
2023-01-25 11:16:40 +0300

    added the first section of the page with heading and paragraph

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -1,7 +1,18 @@
 const Restaurant = () => {
     return (  
         <div className="restautant">
-            
+          <section class="home" id="home">
+
+        <div class="content">
+            <h3>food made with love</h3>
+            <p>Mahali Mzuri Restaurant is one of Nairobi's most iconic food joints . Its website is a feast for the senses, including bright colors, cheeky line illustrations and mouthwatering food photography. Despite how loud and eye-catching the design system is, the site itself is clean, well-structured and easy to navigate. It shows that restaurant websites do not need to choose between making a splash and being user-friendly.</p>
+        </div>
+
+        <div class="image">
+            <img src="images/home-img.png" alt="" />
+        </div>
+
+     </section>
         </div>
     );
 }
