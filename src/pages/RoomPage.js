606b0941a049ada7cb81d57cb2b0ba534b58e9fe
Mutaga Kihahu <kihahumutagah@gmail.com>
2023-03-02 23:34:31 +0300

    Milestone : Added responsiveness to the room page.

--- src/pages/RoomPage.js
+++ src/pages/RoomPage.js
@@ -11,9 +11,9 @@ export default function RoomPage() {
   }, []);
   return (
     <main>
-      <div style={{ backgroundColor: "#f0f0f0" }}>
+      <div style={{ backgroundColor: "#e0e0f0" }}>
         <div className="row text-center py-3">
-          <h3>Our Rooms</h3>
+          <h1 style={{ fontWeight: "1200" }}>Our Rooms</h1>
         </div>
         <div className="row text-center">
           <h1 style={{ fontWeight: "700", fontSize: "bold" }}>
@@ -36,68 +36,102 @@ export default function RoomPage() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
-        <div
-          className="row mx-3"
-          style={{
-            disply: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div className="col-md-4">
-            {roomTypes.map((roomType, index) => {
-              if (index === 0 || index === 1) {
+        <div className="row mx-3 py-3">
+          <div className="col-md-4 d-block justify-content-center">
+            {roomTypes
+              .filter((element, index) => index === 0 || index === 1)
+              .map((roomType) => {
                 return (
-                  <div
-                    className="col-md-4 text-center"
-                  >
-                    <img
-                      className="rounded-5"
-                      src={roomType.image1}
-                      alt="View Hotel"
-                      style={{ maxHeight: "600px", maxWidth: "600px" }}
-                    />
-                    <h4>{roomType.room_type}</h4>
-                  </div>
+                  <>
+                    <div className="row text-center">
+                      <img
+                        className="rounded-5 img-fluid"
+                        src={roomType.image1}
+                        alt="View Hotel"
+                      />
+                    </div>
+                    <div className="row d-flex justify-content-center">
+                      <div
+                        className="col-10 rounded-5 d-flex justify-content-center my-4 align-items-center py-2"
+                        style={{
+                          border: "2px solid #121212",
+                          backgroundColor: "#f17a12",
+                        }}
+                      >
+                        <h4>{roomType.room_type}</h4>
+                      </div>
+                    </div>
+                  </>
                 );
-              }
-            })}
+              })}
           </div>
-          <div className="col-md-4">
-            {roomTypes.map((roomType, index) => {
-              if (index === 2) {
+          <div
+            className="col-md-4 d-flex flex-column p-2 rounded-5"
+            style={{ backgroundColor: "#f17a12" }}
+          >
+            {roomTypes
+              .filter((element, index) => index === 2)
+              .map((roomType) => {
                 return (
-                  <div className="col-md-4" style={{justifyContent:"center",display:"block"}}>
-                    <img
-                      className="rounded-5"
-                      src={roomType.image4}
-                      alt="View Hotel"
-                      style={{ maxWidth: "600px", maxHeight: "500px" }}
-                    />
-                    <p>{roomType.room_type}</p>
-                  </div>
+                  <>
+                    <div className="row p-3">
+                      <img
+                        className="img-fluid rounded-5"
+                        src={roomType.image1}
+                        alt="View Hotel"
+                      />
+                    </div>
+                    <div className="row d-flex justify-content-center">
+                      <div
+                        className="col-10 rounded-5 d-flex justify-content-center my-4 align-items-center py-2"
+                        style={{
+                          border: "2px solid #121212",
+                          backgroundColor: "black",
+                        }}
+                      >
+                        <h4 style={{ color: "#f17a12" }}>
+                          {roomType.room_type}
+                        </h4>
+                      </div>
+                    </div>
+                    <div className="row p-3">
+                      <img
+                        className="img-fluid rounded-5"
+                        src={roomType.image2}
+                        alt="View Hotel"
+                      />
+                    </div>
+                  </>
                 );
-              }
-            })}
+              })}
           </div>
-          <div className="col-md-4">
-            {roomTypes.map((roomType, index) => {
-              if (index === 3 || index === 4) {
+          <div className="col-md-4 d-block justify-content-center">
+            {roomTypes
+              .filter((element, index) => index === 3 || index === 4)
+              .map((roomType) => {
                 return (
-                  <div
-                    className="col-md-4"
-                  >
-                    <img
-                      className="rounded-5"
-                      src={roomType.image1}
-                      alt="View Hotel"
-                      style={{ maxHeight: "600px", maxWidth: "600px" }}
-                    />
-                    <p>{roomType.room_type}</p>
-                  </div>
+                  <>
+                    <div className="row">
+                      <img
+                        className="img-fluid rounded-5 "
+                        src={roomType.image1}
+                        alt="View Hotel"
+                      />
+                    </div>
+                    <div className="row d-flex justify-content-center">
+                      <div
+                        className="col-10 rounded-5 d-flex justify-content-center my-4 align-items-center py-2"
+                        style={{
+                          border: "2px solid #121212",
+                          backgroundColor: "#f17a12",
+                        }}
+                      >
+                        <h4>{roomType.room_type}</h4>
+                      </div>
+                    </div>
+                  </>
                 );
-              }
-            })}
+              })}
           </div>
         </div>
       </div>
